fa2dc596df57859c51b70f2b6af0424bf5be4495
Het0121 <hetprajapati72@gmail.com>
2025-03-27 02:06:51 +0530

    Update theme colors for improved consistency and readability

--- src/Theme/theme.js
+++ src/Theme/theme.js
@@ -1,18 +1,18 @@
 export const theme = {
   colors: {
-    primary: "#fff",
-    main: "#003049",
+    primary: "#FFF",
+    main: "#0C0F14",
     // main: "#3772FF",
-    orange: "#fca311",
-    black: "#2B2B2B",
-    blue: "#4A90E2",
-    bg6: "#f5f5f5",
-    white: "#ffffff",
+    orange: "#FCA311",
+    black: "#0C0F14",
+    blue: "#1F51FF",
+    bg6: "#F5F5F5",
+    white: "#FFFFFF",
     gray: "#B3B3B3",
     subtext: "#666666",
     title: "#333333",
   },
   border: {
-    primary: "#14213d",
+    primary: "#14213D",
   },
 };
